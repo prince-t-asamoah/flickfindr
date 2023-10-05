2ec459b5cb3a28b5ffc0bf6b785b861c293b9712
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-10-05 12:46:51 +0000

    add header for homepage

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import './App.css';
 
 export default function App() {
     return (
-        <div>
-            <h1>Flickfindr</h1>
-            <h2>Discover Cinematic Treasures</h2>
+        <div className="h-screen flex justify-center">
+            <header className="flex flex-col item-center mt-40 md:mt-60">
+                <h1 className="text-deep-navy font-cursive text-3xl tracking-widest">
+                    Flickfindr
+                </h1>
+                <h2 className="text-sm text-steel-gray">
+                    Discover Cinematic Treasures
+                </h2>
+            </header>
         </div>
     );
 }
