97989f4ab614febcd3cab7a8925c591536b04313
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-10-06 10:11:31 +0000

    add search page route

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.tsx';
+import SearchPage from './pages/SearchPage.tsx';
 
-const router = createBrowserRouter([{ path: '/', element: <App /> }]);
+const router = createBrowserRouter([
+    { path: '/', element: <App /> },
+    { path: '/search', element: <SearchPage /> },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <RouterProvider router={router} />
