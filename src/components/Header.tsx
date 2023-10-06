dec1240eb52b31ce795d079de72ba1b9626c3ceb
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-10-06 09:38:31 +0000

    update header padding and heading text size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
-        <header className="text-xl py-5 px-8 lg:py-10 ">
-            <h1 className="text-white font-cursive lg:text-4xl tracking-widest lg:hover:text-gray-400 lg:hover:transition-colors lg:hover:duration-500">
+        <header className="py-5 px-8 lg:py-8">
+            <h1 className="text-white font-cursive text-xl lg:text-2xl tracking-widest lg:hover:text-gray-400 lg:hover:transition-colors lg:hover:duration-500">
                 <Link to="/" title="Home">
                     Flickfindr
                 </Link>
